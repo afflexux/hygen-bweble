1f8353435d1df85575317e3586400b0221c11581
afflexux <afflexux@gmail.com>
2019-07-23 01:55:53 +0100

    added import into component generator

--- _templates/generator/import/inscript.ejs.t
+++ _templates/generator/import/inscript.ejs.t
@@ -3,4 +3,4 @@ inject: true
 to: "<%=dir%>/<%=parent%>.vue"
 after: "<script>"
 ---
-      import <%= h.changeCase.pascal(name) %> from './components/<%=dir%>/<%= h.changeCase.pascal(name) %>.vue'
+      import <%= h.changeCase.pascal(name) %> from './components/<%=subdir%>/<%= h.changeCase.pascal(name) %>.vue'
--- _templates/generator/import/prompt.js
+++ _templates/generator/import/prompt.js
@@ -1,7 +1,7 @@
 module.exports = [{
   type: 'input',
   name: 'name',
-  message: 'Components name:',
+  message: 'Components name your importing. ie: app button',
   validate(value) {
     if (!value.length) {
       return 'Components must be the right name!'
@@ -13,10 +13,10 @@ module.exports = [{
   type: 'input',
   name: 'dir',
   initial: 'src',
-  message: 'The directory of the component your in:',
+  message: 'The directory of the component your importing into. ie: src/components/modal',
   validate(value) {
     if (!value.length) {
-      return 'You must choose the dir of the component your in!'
+      return 'You must choose the dir of the component your importing into!'
     }
     return true
   },
@@ -24,13 +24,18 @@ module.exports = [{
 {
   type: 'input',
   name: 'parent',
-  message: 'Your present component name:',
+  message: 'The name of the component your importing into. ie: modal card ',
   initial: 'App',
   validate(value) {
     if (!value.length) {
-      return 'You must choose the component your in!'
+      return 'You must choose the name of the component your importing into!'
     }
     return true
   },
+},
+{
+  type: 'input',
+  name: 'subdir',
+  message: 'The sub dir of /components where your new component lives. ie: button ',
 },
 ]
